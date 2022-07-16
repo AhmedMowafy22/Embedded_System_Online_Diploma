459ee37868f0a54ae44a06afd8b8351bbc0601e2
Your Name <you@example.com>
2022-07-16 17:13:22 +0200

    C Program

---
+++ unit(2)_C_Programming/1_C_Basics/Assignment(1)/EX(5)_Find_ASCII_Value_of_Character.c
@@ -0,0 +1,20 @@
+/*
+ * EX(5)_Find_ASCII_Value_of_Character.c
+ *
+ *  Created on: Jul 16, 2022
+ *
+ *      Author: Ahmed Mowafy
+ */
+
+#include <stdio.h>
+int main(int argc,char**argv){
+	char c;
+	printf("enter a character :");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&c);
+	printf(" ascii value of (%c)is: %d ",c,c);
+
+
+
+
+}
