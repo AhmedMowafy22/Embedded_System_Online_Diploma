cebd88ebc839098b27320e6ad72516f22068577a
Your Name <you@example.com>
2022-07-16 16:45:31 +0200

    C Program

---
+++ unit(2)_C_Programming/1_C_Basics/Assignment(1)/EX(3)_Add_Two_Integers.c
@@ -0,0 +1,26 @@
+/*
+ * EX(3)_Add_Two_Integers.c
+ *
+ *  Created on: Jul 16, 2022
+ *      Author: Ahmed Mowafy
+ */
+
+#include "stdio.h"
+int main()
+{
+int x,y,sum;
+	printf("enter two integers:");
+	fflush(stdin);fflush(stdout);
+	scanf("%d %d",&x,&y);
+	sum=x+y;
+	printf("sum:%d",sum);
+
+
+
+
+
+
+
+
+
+}
