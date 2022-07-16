f6364654946642f7ae3a08d88b1097553ad6b1b4
Your Name <you@example.com>
2022-07-16 17:45:43 +0200

    C Program

---
+++ unit(2)_C_Programming/1_C_Basics/Assignment(1)/EX(7)_Swap_Two_Numbers_without_temp_variable.c
@@ -0,0 +1,27 @@
+/*
+ * EX(7)_Swap_Two_Numbers_without_temp_variable.
+ *
+ *  Created on: Jul 16, 2022
+ *
+ *      Author: Ahmed Mowafy
+ */
+
+
+#include <stdio.h>
+int main (int argc,char **argv)
+{
+	float a ,b;
+	printf("enter value of a :");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&a);
+
+	printf("enter value of b :");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&b);
+
+	printf("after swapping,value of a :%.2f\n",b);
+	printf("after swapping,value of b :%.1f",a);
+
+
+
+}
