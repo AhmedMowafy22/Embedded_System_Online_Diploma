1a6934467d4d75d3ac7723abe43794a0884bb44b
Your Name <you@example.com>
2022-07-16 15:17:00 +0200

    C Program

---
+++ unit(2)_C_Programming/1_C_Basics/Assignment(1)/EX(1).c
@@ -0,0 +1,15 @@
+/*
+ * EX(1).c
+ *  (Write C Program to Print a Sentence)
+ *
+ *  Created on: Jul 16, 2022
+ *
+ *      Author: Ahmed Mowafy
+ */
+
+
+#include <stdio.h>
+int main (int argc,char**argv)
+{
+	printf("C Programming");
+}
